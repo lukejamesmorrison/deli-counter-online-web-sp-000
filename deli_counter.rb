dfdc30efa354a132d0a723faeef411a1570a8345
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 19:21:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,12 @@ def line(patrons)
   end
 end
 
+
 def take_a_number(patrons, name)
   patrons << name
   puts "Welcome, #{name}. You are number #{patrons.length} in line."
 end
+
 
 def now_serving(patrons)
   if patrons.empty?
